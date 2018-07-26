3c5e6eba3ec02827ee789c44e745bc7aabb97f3c
sugirin <sugirin3621@gmail.com>
2018-07-26 18:13:49 +0900

    tutorial 3 is done

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,4 +1,4 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 import json
 
 from asgiref.sync import async_to_sync
@@ -33,3 +33,33 @@ class ChatConsumer(WebsocketConsumer):
     message = event['message']
 
     self.send(text_data=json.dumps({'message': message}))
+
+class AsyncChatConsumer(AsyncWebsocketConsumer):
+  async def connect(self):
+    self.room_name = self.scope['url_route']['kwargs']['room_name']
+    self.room_group_name = 'chat_{}'.format(self.room_name)
+
+    await self.channel_layer.group_add(
+      self.room_group_name, self.channel_name)
+
+    await self.accept()
+
+  async def disconnect(self, close_code):
+    await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+  async def receive(self, text_data):
+    text_data_json = json.loads(text_data)
+    message = text_data_json['message']
+
+    await self.channel_layer.group_send(
+      self.room_group_name,
+      {
+        'type': 'chat_message',
+        'message': message
+      }
+    )
+
+  async def chat_message(self, event):
+    message = event['message']
+
+    await self.send(text_data=json.dumps({'message': message}))
