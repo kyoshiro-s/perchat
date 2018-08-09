54991c8e548a5bd005b11754ff96b0595befb20b
sugirin <sugirin3621@gmail.com>
2018-08-09 14:31:46 +0900

    match making works well!!

--- chat/routing.py
+++ chat/routing.py
@@ -4,4 +4,5 @@ from . import consumers
 
 websocket_urlpatterns = [
   url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.AsyncChatConsumer),
+  url(r'^ws/matching/(?P<room_name>[^/]+)/$', consumers.AsyncMatchingConsumer),
 ]
