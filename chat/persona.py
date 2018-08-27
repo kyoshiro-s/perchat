490a373df97b5497e6701d9bbb4c3a2f8ea9f360
sugirin <sugirin3621@gmail.com>
2018-08-27 16:11:58 +0900

    add persona generation. a matching bug is found.

---
+++ chat/persona.py
@@ -0,0 +1,15 @@
+from random import choice, sample
+def generate_random():
+  num_attr = 5
+  attribute_list = [
+    ('姓', ['田中', '佐藤', '鈴木']),
+    ('名', ['太郎', '花子', '次郎', '葵']),
+    ('性別', ['男', '女']),
+    ('年齢', [str(i) for i in range(9, 50)]),
+    ('職業', ['無職', '学生', '会社員', '自営業']),
+    ('所属', ['なし', 'A校', 'B社']),
+    ('趣味', ['なし', '読書', 'ゲーム', 'アウトドア', 'スポーツ観戦']),
+    ('居住地', ['東京', '大阪', '京都', '福岡', '広島']),
+  ]
+
+  return {a[0]: choice(a[1]) for a in sample(attribute_list, num_attr)}
--- chat/views.py
+++ chat/views.py
@@ -4,6 +4,7 @@ import json
 import random
 import string
 from uuid import uuid4
+from .persona import generate_random
 
 # Create your views here.
 matching_queue = []
@@ -14,8 +15,8 @@ def index(request):
     matching_queue.append(room_name)
   else:
     room_name = matching_queue.pop()
-  # room_name_dict = {'room_name': room_name}
-  return render(request, 'chat/index.html', {'room_name': mark_safe(room_name)})
+  persona = generate_random()
+  return render(request, 'chat/index.html', {'room_name': mark_safe(room_name), 'persona': persona})
 
 def room(request, room_name, turn):
   print(request.POST)
