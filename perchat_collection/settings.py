4f04adb6c210d62649d680a2ee8d7a80f37cc2fc
sugirin <sugirin3621@gmail.com>
2018-09-25 15:37:19 +0900

    localでは動いた

--- perchat_collection/settings.py
+++ perchat_collection/settings.py
@@ -118,30 +118,31 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
-
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-STATIC_ROOT = 'staticfiles'
-
-ASGI_APPLICATION = 'perchat_collection.routing.application'
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {'hosts': [('localhost', 6379)]},
-    },
-}
-
 try:
     from .local_settings import *
+    print('USING LOCAL SETTINGS')
 except ImportError:
-    pass
+    STATIC_URL = '/static/'
+    STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
+
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    ALLOWED_HOSTS = ['*']
+
+    STATIC_ROOT = 'staticfiles'
+
+    ASGI_APPLICATION = 'perchat_collection.routing.application'
+    CHANNEL_LAYERS = {
+        'default': {
+            'BACKEND': 'channels_redis.core.RedisChannelLayer',
+            'CONFIG': {'hosts': [('localhost', 6379)]},
+        },
+    }
+    print('NOT USING LOCAL SETTINGS')
+
 
 if not DEBUG:
     import django_heroku
